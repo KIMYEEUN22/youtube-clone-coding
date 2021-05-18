dabf4dab2d6227c6fc5b958d577d3b17543af9bf
yeeun <kye9702@naver.com>
2021-05-18 16:07:57 +0900

    popular list

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import './app.css';
+import React, { useEffect, useState } from "react";
+import "./app.css";
+import VideoList from "./compoents/video_list/video_list";
 
 function App() {
-  return(  
-  <h1>Hello :)</h1>
-  )
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?key=AIzaSyAD6OwbwNDmOemjFq6v52VfXwoh2M2uTLg&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAD6OwbwNDmOemjFq6v52VfXwoh2M2uTLg",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
